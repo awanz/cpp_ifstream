3df4ca609c9bb656eef4240090444f9e9a0aca57
awan <me.awan.es@gmail.com>
2022-11-04 06:57:57 +0700

    add check file can open or not empty

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
     char st[255];
     ifstream f;
     f.open("awan.txt");
-    while(!f.eof()){
-        f.getline(st, 255);
-        cout << st << endl;
+    if(f.peek() > 0){
+        while(!f.eof()){
+            f.getline(st, 255);
+            cout << st << endl;
+        }
+    }else{
+        cout << "File can't open or empty" << endl;
     }
     f.close();
     return 0;
